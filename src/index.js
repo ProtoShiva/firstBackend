091114848151516713aefec499dcc0bbcf78411a
Himanshu <hgusain088@gmail.com>
2024-01-19 12:30:51 +0530

    db connection to mongoAtlas

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+// import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// const app = express()(async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR!: cannot talk with express", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, ()=>{
+//       console.log(`Server listening at ${process.env.PORT}`);
+//     })
+//   } catch (error) {
+//     console.log("ERROR!: ", error.message);
+//   }
+// })();
